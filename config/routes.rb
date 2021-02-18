43387d1e9d7f5a87257ffb14272a71503e152887
hapi190601 <gc1206ro@gmail.com>
2021-02-18 09:04:31 +0000

    [0218-1800]

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   get "/about" => "homes#about"
 
   resources :users, only: [:index, :show, :edit, :update]
-  resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
 end
