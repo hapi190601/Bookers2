ec77d0678cb4ffac45ff8f880f3045089eae9327
hapi190601 <gc1206ro@gmail.com>
2021-02-20 05:39:47 +0000

    [Finish]4-10

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: 'users/registrations', sessions: 'users/sessions' }
 
   root to: "homes#top"
-  get "/about" => "homes#about"
+  get "home/about" => "homes#about"
 
   resources :users, only: [:index, :show, :edit, :update]
   resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
